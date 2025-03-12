5dc5362e85b6535efbb77cde43728babb433bf46
Irenge Bahizire <eshibahizire@gmail.com>
2025-03-13 00:03:40 +0800

    link6

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -7,7 +7,7 @@ import {format} from "timeago.js";
 import FavoriteSharpIcon from '@mui/icons-material/FavoriteSharp';
 import MoreVertSharpIcon from '@mui/icons-material/MoreVertSharp';
 const Container = styled.div`
-  width: ${(props) => props.type !== "sm" && "360px"};
+  width: ${(props) => props.type !== "sm" && "380px"};
   margin-bottom: ${(props) => (props.type === "sm" ? "10px" : "45px")};
   cursor: pointer;
   display: ${(props) => props.type === "sm" && "flex"};
@@ -81,6 +81,10 @@ const Card = ({ type, video }) => {
   setmycpy('Copied')
   couper()
   } 
+  if (Op) {
+    window.onclick = couper; // Assign function reference correctly
+}
+
   return (
     <Link to={`/video/test?id=${video._id}`} style={{ textDecoration: "none" }}>
       <Container type={type}>
@@ -100,7 +104,7 @@ const Card = ({ type, video }) => {
             <Info>{video.view} views • {format(video.createdAt)}</Info>
           </Texts>
         </Details>
-        <div className='myb32'><MoreVertSharpIcon className='myb321' onClick={()=>setOp(true)}/>{Op && <div style={{position:'absolute',marginLeft:'-59px',marginTop:'-7px'}}><div className='op' onClick={()=>copyf(`https://irengetubefront.vercel.app/video/test?id=${video.link}`)} style={{border:'2px solid black',padding:'3px',textAlign: 'center',alignItems:'center' }}>{mycpy}</div></div>}</div>
+        <div className='myb32'><MoreVertSharpIcon  className='myb321' style={{color:'white'}} onClick={()=>setOp(true)}/>{Op && <div style={{position:'absolute',marginLeft:'-59px',marginTop:'-7px'}}><div className='op' onClick={()=>copyf(`https://irengetubefront.vercel.app/video/test?id=${video.link}`)} style={{border:'2px solid black',padding:'3px',textAlign: 'center',alignItems:'center' }}>{mycpy}</div></div>}</div>
       </div>
       </Container>
     </Link>
